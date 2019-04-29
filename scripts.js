d56fc0f3c3e891affe3a3442bfbc6af9903efc2b
Sven Seeberg <mail@sven-seeberg.de>
2019-04-29 17:25:58 +0200

    Implement fair language API

--- scripts.js
+++ scripts.js
@@ -3,10 +3,12 @@ $('#cities').on('change', function() {
     $.getJSON( "https://cms-test.integreat-app.de/"+ this.value +"/de/wp-json/extensions/v3/pages", function( data ) {
 		var options = "";
         $.each( data, function( key, page ) {
-            options = options.concat( "<option value='" + page.url + "'>" + page.title + "</option>" );
+            options = options.concat( "<option value='" + page.id + "'>" + page.title + "</option>" );
+            localStorage.setItem("ig-page-" + page.id, page.content);
+            localStorage.getItem("ig-page-" + page.id);
         });
         $('#pages').html(options);
-        localStorage.setItem("ig-pages", data);
+
     });
 });
 
@@ -20,3 +22,41 @@ $( document ).ready(function() {
     });
 });
 
+function parse_xml( xml ) {
+    //console.log(xml);
+    var element = xml.getElementsByTagName("results")[0];
+    //console.log(element);
+    var children = element.childNodes;
+    //console.log(children);
+    var html = "";
+    var errors = Array.from(children);
+    for (let error in errors) {
+        var word = errors[error].querySelector('string').textContent;
+        var type = errors[error].querySelector('description').textContent;
+        var precontext = errors[error].querySelector('precontext').textContent;
+        var suggestions = Array.from(errors[error].querySelector('suggestions').children);
+        var options = ["a", "b"];
+        for (let option in suggestions) {
+            options.push( suggestions[option].textContent );
+        }
+        html = html + "<div><h2>" + word + "</h2>" + type + " &mdash; " +
+            "Kontext: " + precontext + "<br>Alternativen: " +
+            options.join(", ") + "</div>";
+    }
+    return html;
+}
+
+$('#check').on('click', function() {
+    var page_id = $("#pages").val();
+    var storage_key = "ig-page-" + page_id;
+    var page = localStorage.getItem(storage_key);
+    $.ajax({
+        type: "POST",
+        url: "https://fairlanguage-api-dev.dev-star.de/checkDocument",
+        data: { 'data': page },
+        success: function( data ) {
+            $("#result").html(parse_xml(data));
+            $("#result").css("display", "block");
+        }
+    });
+});
